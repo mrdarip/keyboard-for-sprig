e650bc72aebbe9aa99817d4b8e4806fba901106d
mrdarip <darioelas2005@gmail.com>
2024-07-26 20:43:42 +0200

    Added c# keywords as words

--- keyboardForSprig.js
+++ keyboardForSprig.js
@@ -5,7 +5,7 @@
 @addedOn: 2024-00-00
 */
 
-const words = ("hello world take some words for practicing writting on sprig").toLowerCase().split(" ")
+const words = ("abstract as base bool break byte case catch char checked class const continue decimal default delegate do double else enum event explicit extern false finally fixed float for foreach goto if implicit in int interface internal is lock long namespace new null object operator out override params private protected public readonly ref return sbyte sealed short sizeof stackalloc static string struct switch this throw true try typeof uint ulong unchecked unsafe ushort using virtual void volatile while add alias ascending async await by descending dynamic equals from get global group into join let nameof notnull on orderby partial remove select set unmanaged value var when where yield").toLowerCase().split(" ")
 
 const keys = ("wdsailkj").split("")
 
